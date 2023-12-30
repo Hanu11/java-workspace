6345b88db7e3678ae7a69dba869373fb2b495c65
Hanu <hanumantha.rao@outlook.com>
2023-12-30 17:52:26 +0530

    many changes

---
+++ currency-exchange-service/src/main/java/com/hanu/microservices/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,8 @@
+package com.hanu.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository 
+	extends JpaRepository<CurrencyExchange, Long> {
+	CurrencyExchange findByFromAndTo(String from, String to);
+}
